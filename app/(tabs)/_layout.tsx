8e2ab71624dfe1d2102e4629c8ab49f97cba2d96
Ruojie HAO <92860071+hrjlhy123@users.noreply.github.com>
2025-06-12 14:02:34 -0700

    debug

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -39,7 +39,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="intro"
+        name="index"
         options={{
           title: "Home",
           tabBarIcon: ({ color }) => (
